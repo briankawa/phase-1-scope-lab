c854f8e52c0d4f269d05b8b398c3f7f21a04c38b
Brian <brian.kawa@student.moringaschool.com>
2023-10-02 10:43:56 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer(){
+throw new Error('Assignment to constant variable.');
+}
